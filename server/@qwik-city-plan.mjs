c5ac1af5ec11ad6c407656d21f02bc2485358d8b
notcod <mneca99+git@gmail.com>
2023-10-19 08:00:00 +0200

    serve

---
+++ server/@qwik-city-plan.mjs
@@ -0,0 +1,1 @@
+import{w as e,x as r,q as t,p as l,r as o,o as u,t as n}from"./assets/@qwik-city-plan-3cb33df5.mjs";export{e as basePathname,r as cacheModules,t as default,l as menus,o as routes,u as serverPlugins,n as trailingSlash};
---
+++ server/entry.ssr.mjs
@@ -0,0 +1,1 @@
+import"./assets/@qwik-city-plan-3cb33df5.mjs";import{r as a}from"./assets/entry.ssr-82d3bc3f.mjs";export{a as default};
